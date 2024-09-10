8cfdb37b5085d84ec1fa7f1d32b1419a71e5bae1
YenNhi200902 <nhiyen200802@gmail.com>
2024-09-10 08:21:34 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Vibration,
+} from 'react-native';
+import {useState} from 'react';
+import AntDesign from '@expo/vector-icons';
+export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [currentNumber, setCurrentNumber] = useState('');
+  const [lastNumber, setLastNumber] = useState('');
+  const buttons = [
+    'C',
+    'DEL',
+    '/',
+    7,
+    8,
+    9,
+    '*',
+    4,
+    5,
+    6,
+    '-',
+    1,
+    2,
+    3,
+    '+',
+    0,
+    '.',
+    '=',
+  ];
+  function calculator() {
+    let lastArr = currentNumber[currentNumber.length - 1];
+    if (
+      lastArr === '/' ||
+      lastArr === '*' ||
+      lastArr === '-' ||
+      lastArr === '+' ||
+      lastArr === '.' 
+    ) {
+      setCurrentNumber(currentNumber);
+      return;
+    } else {
+      let result = eval(currentNumber).toString()
+      setCurrentNumber(result)
+      return
+  }
+}
